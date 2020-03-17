5e3aaa4d2fe32dda3a71d0217db2f89d2d6b31a6
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-03-17 15:43:24 -0600

    updated to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
+require("dotenv").config();
 const PORT = process.env.PORT || 19003;
 
 const app = require("./application");
 const server = require("http").Server(app);
 
+function read(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      file,
+      {
+        encoding: "utf-8"
+      },
+      (error, data) => {
+        if (error) return reject(error);
+        resolve(data);
+      }
+    );
+  });
+}
+
+// if (ENV === "development" || ENV === "test") {
+  Promise.all([
+    read(path.resolve(__dirname, `db/schema/data.sql`)),
+    read(path.resolve(__dirname, `db/seeds/data.sql`))
+  ])
+    .then(([create, seed]) => {
+      app.get("/api/debug/reset", (request, response) => {
+        db.query(create)
+          .then(() => db.query(seed))
+          .then(() => {
+            console.log("Database Reset");
+            response.status(200).send("Database Reset");
+          });
+      });
+    })
+    .catch(error => {
+      console.log(`Error setting up the reset route: ${error}`);
+    });
+// }
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+

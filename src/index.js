5fab1c81a9bad0d7fd8766e8d2ed2fedf1359d25
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-19 11:28:37 -0600

    update the master in my local terminal

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const PORT = process.env.PORT || 19003;
 
 const app = require("./application");
 const server = require("http").Server(app);
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+(async() => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'usd',
+  });
+  const clientSecret = paymentIntent.client_secret;
+  // Pass the client secret to the client
+
+})();
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);

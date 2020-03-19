09f9fc32d6c67954774691ce3942688ef1bc8a3e
qsaleh <qmsaleh@gmail.com>
2020-03-19 21:28:09 +0000

    updated schema and seeds

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const getProduct = require('../db/queries');
 
 module.exports = db => {
-  router.get("/product-details", (request, response) => {
-    db.query(`SELECT * FROM products;`)
+  router.get("/product-details/:id", (request, response) => {
+    db.query(`SELECT * FROM products WHERE upc = 'id';`)
       .then(({ rows: getProduct }) => {
         response.json(getProduct);
       });

7dd2f017a7c3181681562a620eaa0b8b66b67bb2
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-03-21 14:42:44 -0600

    added new item

--- src/db/seeds/data.sql
+++ src/db/seeds/data.sql
@@ -11,6 +11,9 @@ VALUES ('Notebook', '3 subject notebook', 4, 667888093731, 'picture', true, fals
 INSERT INTO products (name, description, price, UPC, picture, recyclable, compostable)
 VALUES ('Axe Black', 'Aerosol deodorant', 7, 079400641670, 'picture', false, false);
 
+INSERT INTO products (name, description, price, UPC, picture, recyclable, compostable)
+VALUES ('Vaseline', 'Extremely Dry Skin Rescue', 6, 305210042247, 'picture', false, false);
+
 -- options table seeds here (Example)
 INSERT INTO orders (user_id, date_created, total) VALUES (1, NOW(), 5);
 

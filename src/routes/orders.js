89a06820689136f16073cd0a5d752b3a7b616b5e
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-22 16:58:22 -0600

    fixed the query for orders

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const getOrders = require("../db/queries");
 module.exports = (db) => {
   router.get("/orders", (request, response) => {
-    db.query(`SELECT quantity, product_id, order_id FROM products_orders;`)
+    db.query(`SELECT name, products_orders.price, quantity FROM products JOIN products_orders ON products.id = products_orders.product_id;`)
       .then(({ rows: orders }) => {
         console.log(orders, "is it accessing here?");
         return response.json(orders);

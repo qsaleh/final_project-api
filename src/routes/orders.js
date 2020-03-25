84021cc64b652885e565a76a9a378ac4eb8e3457
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-24 20:36:11 -0600

    added routes

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -2,10 +2,56 @@ const router = require("express").Router();
 const getOrders = require("../db/queries");
 module.exports = (db) => {
   router.get("/orders", (request, response) => {
-    db.query(`SELECT quantity, product_id, order_id FROM products_orders;`)
+    db.query(`SELECT * FROM orders;`)
       .then(({ rows: orders }) => {
         console.log(orders, "is it accessing here?");
         return response.json(orders);
+      })
+      .catch(e => {
+        console.log(e.message);
+        response.json({ error: true });
+      });
+  });
+  router.post("/orders", (request, response) => {
+    db.query(`
+    INSERT INTO orders (user_id, date_created, total)
+    VALUES (1, Now(), 6)
+    RETURNING *;
+    `)
+      .then(({ rows: orders }) => {
+        console.log(orders, "is it accessing here?");
+        console.log(request)
+        return response.json(orders);
+      })
+      .catch(e => {
+        console.log(e.message);
+        response.json({ error: true });
+      });
+  });
+  router.get("/products-orders", (request, response) => {
+    db.query(`
+    SELECT * FROM products_orders;
+    `)
+      .then(({ rows: productsOrders }) => {
+        console.log(productsOrders, "is it accessing here?");
+        console.log(request)
+        return response.json(productsOrders);
+      })
+      .catch(e => {
+        console.log(e.message);
+        response.json({ error: true });
+      });
+  });
+  router.post("/products-orders", (request, response) => {
+    db.query(`
+    INSERT INTO products_orders (quantity, product_id, order_id)
+    VALUES (1, 2, 6)
+    RETURNING *;
+    `)
+      .then(({ rows: productsOrders }) => {
+        console.log(request)
+        console.log(productsOrders, "is it accessing here?");
+        return response.json(productsOrders);
       })
       .catch(e => {
         console.log(e.message);

7cf2f5976784bb42dd57773a2835dd1138ba7701
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-25 14:28:52 -0600

    consolidated into one end point

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -5,6 +5,7 @@ module.exports = (db) => {
     db.query(`SELECT * FROM orders;`)
       .then(({ rows: orders }) => {
         console.log(orders, "is it accessing here?");
+        console.log(request)
         return response.json(orders);
       })
       .catch(e => {
@@ -13,51 +14,35 @@ module.exports = (db) => {
       });
   });
   router.post("/orders", (request, response) => {
-    db.query(`
-    INSERT INTO orders (user_id, date_created, total)
-    VALUES (1, Now(), 6)
-    RETURNING *;
-    `)
-      .then(({ rows: orders }) => {
-        console.log(orders, "is it accessing here?");
-        console.log(request)
-        return response.json(orders);
-      })
-      .catch(e => {
-        console.log(e.message);
-        response.json({ error: true });
-      });
+    console.log(request)
+    // db.query(`
+    // INSERT INTO orders (user_id, date_created, total)
+    // VALUES (1, Now(), 6)
+    // RETURNING *;
+    // `)
+    //   .then(({ rows: orders }) => {
+    //         db.query(`
+    // INSERT INTO products_orders (quantity, product_id, order_id)
+    // VALUES ($1, 2, 6)
+    // RETURNING *;
+    // `)
+    //   .then(({ rows: productsOrders }) => {
+    //     console.log(productsOrders, "is it accessing here?");
+    //     return response.json(productsOrders);
+    //   })
+    //   .catch(e => {
+    //     console.log(e.message);
+    //     response.json({ error: true });
+    //   });
+    //     console.log(orders, "is it accessing here?");
+    //     return response.json(orders);
+    //   })
+    //   .catch(e => {
+    //     console.log(e.message);
+    //     response.json({ error: true });
+    //   });
   });
-  router.get("/products-orders", (request, response) => {
-    db.query(`
-    SELECT * FROM products_orders;
-    `)
-      .then(({ rows: productsOrders }) => {
-        console.log(productsOrders, "is it accessing here?");
-        console.log(request)
-        return response.json(productsOrders);
-      })
-      .catch(e => {
-        console.log(e.message);
-        response.json({ error: true });
-      });
-  });
-  router.post("/products-orders", (request, response) => {
-    db.query(`
-    INSERT INTO products_orders (quantity, product_id, order_id)
-    VALUES (1, 2, 6)
-    RETURNING *;
-    `)
-      .then(({ rows: productsOrders }) => {
-        console.log(request)
-        console.log(productsOrders, "is it accessing here?");
-        return response.json(productsOrders);
-      })
-      .catch(e => {
-        console.log(e.message);
-        response.json({ error: true });
-      });
-  });
+
 
   return router;
 };

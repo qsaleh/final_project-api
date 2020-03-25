83ce3fbffeb7bf69d582bac3d550d43481d121ac
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-25 15:58:24 -0600

    added express.json function call to indexjs

--- src/application.js
+++ src/application.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const getProductRoute = require("./routes/product");
+app.use(express.json());
 app.use("/api", getProductRoute(db));
 const getOrderRoute = require("./routes/orders");
 app.use("/api", getOrderRoute(db));

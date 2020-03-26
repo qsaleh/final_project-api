d3e468a259a46a071b4076d05ced711e179d8686
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-26 15:02:37 -0600

    aaded images

--- src/application.js
+++ src/application.js
@@ -2,16 +2,13 @@ const express = require("express");
 const db = require("./db");
 const getOrderRoute = require("./routes/orders");
 const getProductRoute = require("./routes/product");
-<<<<<<< HEAD
 
 const app = express();
 
-=======
-app.use(express.json());
->>>>>>> d41043d6e791707fb5f977d3c8771c7ead451c44
 app.use("/api", getProductRoute(db));
 app.use("/api", getOrderRoute(db));
 app.use(express.static('public'))
 
 module.exports = app;
 
+//images/axe

0359bd46fc179ca341c33a0eed61bf5eb12c2437
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-26 19:21:33 -0600

    fix seeds

--- src/application.js
+++ src/application.js
@@ -4,7 +4,7 @@ const getOrderRoute = require("./routes/orders");
 const getProductRoute = require("./routes/product");
 
 const app = express();
-
+app.use(express.json());
 app.use("/api", getProductRoute(db));
 app.use("/api", getOrderRoute(db));
 app.use(express.static('public'))

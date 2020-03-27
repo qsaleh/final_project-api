b8b4d2fd483c4e5e01d91c1653dfc2a2485c23ca
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-26 19:24:46 -0600

    fix seeds

--- src/db/seeds/data.sql
+++ src/db/seeds/data.sql
@@ -14,4 +14,4 @@ VALUES ('Book', 'Traction - Expanded edition', 6, 9781936661831, 'picture', true
 -- options table seeds here (Example)
 INSERT INTO orders (user_id, date_created, total) VALUES (1, NOW(), 5);
 -- voters table seeds here (Example)
-INSERT INTO products_orders (quantity, product_id, order_id) VALUES (2, 1, 1);
+INSERT INTO products_orders (quantity, product_id, order_id) VALUES (2, 1, 1);  

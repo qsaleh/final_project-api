4093e3d0a0a691c824ff1be5fbbc85fd3845d596
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-27 14:35:57 -0600

    added picture to seeds

--- src/db/seeds/data.sql
+++ src/db/seeds/data.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (name, email, password) VALUES ('Alice', 'Alice@mx.com', 'password');
 -- polls table seeds here (Example)
 INSERT INTO products (name, description, price, UPC, picture, recyclable, compostable)
-VALUES ('Tops book', 'executive notebook', 5, 025932253300, 'picture', false, false);
+VALUES ('Tops book', 'executive notebook', 5, 025932253300, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQUP8mEt24tY8pt63mYyollvATlT2mkzHT25JH_KfB3mYW8vb6', false, false);
 INSERT INTO products (name, description, price, UPC, picture, recyclable, compostable)
 VALUES ('Notebook', '3 subject notebook', 4, 667888093731, 'https://cdn.shopify.com/s/files/1/0036/4806/1509/products/m005664459_0730bb46-a5e4-4967-aed7-b17a794e1124_1000x1000@2x.jpg?v=1582279298', true, false);
 INSERT INTO products (name, description, price, UPC, picture, recyclable, compostable)
@@ -15,11 +15,3 @@ VALUES ('Book', 'Traction - Expanded edition', 6, 9781936661831, 'picture', true
 INSERT INTO orders (user_id, date_created, total) VALUES (1, NOW(), 5);
 -- voters table seeds here (Example)
 INSERT INTO products_orders (quantity, product_id, order_id) VALUES (2, 1, 1);
-
--- INSERT INTO products (name, picture)
--- VALUES
--- ('notebook', '/images/notebook.jpg')
-
--- ('Aerosol deodorant', '/images/axe.jpg')
-
--- ('Extremely Dry Skin Rescue', '/images/vaseline.jpg')
